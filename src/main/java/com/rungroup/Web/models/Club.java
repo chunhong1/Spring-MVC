0bf20331424af52c08f44be8a79239e801012392
Yeap-Chun-Hong <chunhong2002@outlook.com>
2025-04-12 21:30:20 +0800

    Setting up database table

---
+++ src/main/java/com/rungroup/Web/models/Club.java
@@ -0,0 +1,31 @@
+package com.rungroup.Web.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "clubs")
+public class Club {
+    @Id
+    private Long id;
+    private String title;
+    private String photoUrl;
+    private String content;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+}
