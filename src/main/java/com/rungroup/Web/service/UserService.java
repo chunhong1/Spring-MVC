8c2fb59d37ca3bbeba39974e8974b2aa33930845
Yeap-Chun-Hong <chunhong2002@outlook.com>
2025-08-31 15:56:47 +0800

    Feat: Register

--- src/main/java/com/rungroup/Web/service/UserService.java
+++ src/main/java/com/rungroup/Web/service/UserService.java
@@ -1,8 +1,13 @@
 package com.rungroup.Web.service;
 
 import com.rungroup.Web.dto.RegistrationDto;
+import com.rungroup.Web.models.UserEntity;
 
 public interface UserService
 {
     void saveUser(RegistrationDto registrationDto);
+
+    UserEntity findByEmail(String email);
+
+    UserEntity findByUsername(String username);
 }
--- src/main/java/com/rungroup/Web/service/impl/UserServiceImpl.java
+++ src/main/java/com/rungroup/Web/service/impl/UserServiceImpl.java
@@ -7,9 +7,11 @@ import com.rungroup.Web.repository.RoleRepository;
 import com.rungroup.Web.repository.UserRepository;
 import com.rungroup.Web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
+@Service
 public class UserServiceImpl implements UserService
 {
     private UserRepository userRepository;
@@ -33,5 +35,17 @@ public class UserServiceImpl implements UserService
         Role role = roleRepository.findByName("USER");
         user.setRoles(Arrays.asList(role));
         userRepository.save(user);
+    }
+
+    @Override
+    public UserEntity findByEmail(String email)
+    {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserEntity findByUsername(String username)
+    {
+        return userRepository.findByUsername(username);
     }
 }
