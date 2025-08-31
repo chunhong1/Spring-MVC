598ab6c8750d088ede1fbef67e06308163736ebe
Yeap-Chun-Hong <chunhong2002@outlook.com>
2025-08-31 21:15:56 +0800

    Feat: User Details Service

--- src/main/java/com/rungroup/Web/service/impl/UserServiceImpl.java
+++ src/main/java/com/rungroup/Web/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.rungroup.Web.repository.RoleRepository;
 import com.rungroup.Web.repository.UserRepository;
 import com.rungroup.Web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -16,12 +17,14 @@ public class UserServiceImpl implements UserService
 {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository)
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder)
     {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -30,7 +33,7 @@ public class UserServiceImpl implements UserService
         UserEntity user = new UserEntity();
         user.setUsername(registrationDto.getUsername());
         user.setEmail(registrationDto.getEmail());
-        user.setPassword(registrationDto.getPassword());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
 
         Role role = roleRepository.findByName("USER");
         user.setRoles(Arrays.asList(role));
